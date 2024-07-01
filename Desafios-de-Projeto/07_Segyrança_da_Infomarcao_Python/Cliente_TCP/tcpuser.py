cbb12463fc322cea83144be310cde6a5332e485c
Rafael Lucas <rafaellucas-ti@hotmail.com>
2024-07-01 20:55:28 -0300

    Segurança da Informação com Python

---
+++ Desafios-de-Projeto/07_Segyrança_da_Infomarcao_Python/Cliente_TCP/tcpuser.py
@@ -0,0 +1,56 @@
+import socket
+import sys
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as e: 
+        print("A conexão falhou!!")
+        print("Error: {}" .format(e))
+        sys.exit()
+        
+    print("Socket criado com sucesso!")
+
+    HostAlvo = input("Digite o Host ou Ip a ser conectado: ")
+    PortaAlvo = input("Digite a porta a ser conectada: ")
+
+    try:
+        s.connect((HostAlvo, int(PortaAlvo)))
+        print("Cliente TCP conectado com sucesso no Host" + HostAlvo + "e na Porta: " + PortaAlvo)
+        s.shutdown(2)
+    except socket.error as e:
+        print("A conexão falhou")
+        print("Error: {}" .format(e))
+        sys.exit()
+
+if __name__ == "__main__":
+     main()
+
+"""import socket
+import sys
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as e: 
+        print("A conexão falhou!!")
+        print("Error: {}" .format(e))
+        sys.exit()
+
+    print("Socket criado com sucesso!")
+    
+    HostAlvo = input("Digite o Host ou IP a ser conectado: ")
+    PortaAlvo = input("Digite a porta a ser conectada: ")
+
+    try:
+        s.connect((HostAlvo, int(PortaAlvo)))
+        print("Cliente TCP conectado com sucesso no Host " + HostAlvo + " e na Porta: " + PortaAlvo)
+        s.shutdown(2)
+    except socket.error as e:
+        print("A conexão falhou")
+        print("Error: {}" .format(e))
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
+"""
